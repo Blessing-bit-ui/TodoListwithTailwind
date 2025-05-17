0e970c389a1b2ac92b72f8252abb4e317558c473
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-17 03:00:06 -0700

    created the handle taskbuton

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -3,8 +3,22 @@ import { useState } from 'react'
 export default function TodoList(){
     const [input, setInput] = useState([]);
     const [tasks, setTasks] = useState(["laundry", "groceries" ]);
+    const [count, setCount] = useState(0)
+
+  function handleTaskbutton(){
+    setTasks( [...tasks, input])
+  }
      
 return(
+<div>
+    <h1 class="text-red-200 ">To-Do List</h1>
+    <input 
+    value={input}
+    placeholder="Enter task"
+    onChange={(e)=> setInput(e.target.value)}
+    />
+    <button onClick={handleTaskbutton}>Add Task</button>
     <h1>{tasks}</h1>
+    </div>
 )
 }
