c7f407a027944dd5b44d809aa8b1627a0938a0be
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-17 06:15:40 -0700

    added tailwind css

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -9,12 +9,18 @@ export default function TodoList(){
     const [completeIndexes, setCompleteIndexes] = useState([])
     const [incompleteIndexes, setInCompleteIndexes] = useState([])
    const [tasksIndexes, setTaskIndexes] = useState([])
+   const [errors, setErrors] =useState("")
     function handleTaskbutton(){
     if(input !== ""){
     setTasks([...tasks, input])
     setCount(count + 1)
     setInput("")
     }
+    if(input ==""){
+    setErrors("Tasks is required")
+    return;
+    }
+    setErrors("")
   }
    function deleteButton(index){
     const newTasks=tasks.filter((_,i) =>i !== index)
@@ -43,35 +49,37 @@ function handleIncomplete(index){
 }
      
 return (
-  <div>
-    <h1 class="text-red-200 ">To-Do List</h1>
+  <div class=" w-100 rounded-lg shadow-md p-6">
+    <h1 class="text-center font-bold text-[30px]">To-Do List</h1>
     <input
       value={input}
       placeholder="Enter task"
       onChange={(e) => setInput(e.target.value)}
+      class=" p-1 border-2 rounded-md w-[260px] mr-4"
     />
-    <button onClick={handleTaskbutton}>Add Task</button>
+    <button onClick={handleTaskbutton} class="bg-black text-white rounded-md p-1">Add Task</button>
+    {errors && <p>{errors}</p>}
     <ul>
       {tasks.map((task, index) => (
-        <li key={index}>
+        <li key={index} class=" w-66 shadow-md p-4">
           {task}{" "}
           <span>
             <button onClick={() => deleteButton(index)}>X</button>
           </span>
           <span>
-            <button onClick={()=>handleComplete(index)}>✅</button>
+            <button onClick={() => handleComplete(index)}>✅</button>
           </span>
           <span>
-            <button onClick={()=>handleIncomplete(index)}>
-            ⭕
-            </button>
+            <button onClick={() => handleIncomplete(index)}>⭕</button>
           </span>
         </li>
       ))}
     </ul>
+    <div class="p-6 ">
     <p> Number of task to be Done: {count}</p>
     <p> Number of Complete Tasks : {complete}</p>
     <p>Number of Incompleted Tasks : {incomplete}</p>
+    </div>
   </div>
 );
 }
