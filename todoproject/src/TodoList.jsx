0beae5cf7ce2074a4a638aeaeb22d12f63a7c72a
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-17 03:36:03 -0700

    create deleteButton

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -3,11 +3,21 @@ import { useState } from 'react'
 export default function TodoList(){
     const [input, setInput] = useState([]);
     const [tasks, setTasks] = useState(["laundry", "groceries" ]);
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(2)
 
   function handleTaskbutton(){
-    setTasks( [...tasks, input])
+    if(input !== ""){
+    setTasks([...tasks, input])
+    setCount(count + 1)
+    setInput("")
+    }
   }
+   function deleteButton(index){
+    const newTasks=tasks.filter((_,i) =>i !== index)
+    setTasks(newTasks)
+    setCount(count - 1)
+    setInput("")
+   }
      
 return(
 <div>
@@ -18,7 +28,12 @@ return(
     onChange={(e)=> setInput(e.target.value)}
     />
     <button onClick={handleTaskbutton}>Add Task</button>
-    <h1>{tasks}</h1>
+    <ul>
+    {tasks.map((task, index)=>(
+        <li key={index}>{task}  <span><button onClick={()=>deleteButton(index)}>X</button></span></li>
+    ))}
+    </ul>
+    <p> Number of task to be Done: {count}</p>
     </div>
 )
 }
