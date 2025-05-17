3ec44525199e0c3e4037497c61045c3b2daa6722
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-17 04:15:08 -0700

    created the handleComplete button

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -4,6 +4,8 @@ export default function TodoList(){
     const [input, setInput] = useState([]);
     const [tasks, setTasks] = useState(["laundry", "groceries" ]);
     const [count, setCount] = useState(2)
+    const[complete, setComplete] = useState(0)
+    const [completeIndexes, setCompleteIndexes] = useState([])
 
   function handleTaskbutton(){
     if(input !== ""){
@@ -18,22 +20,39 @@ export default function TodoList(){
     setCount(count - 1)
     setInput("")
    }
+   function handleComplete(index){
+    if(!completeIndexes.includes(index)){
+        setCount(count - 1)
+        setComplete(complete + 1)
+        setCompleteIndexes([...completeIndexes, index])
+        
+    }
+}
      
-return(
-<div>
+return (
+  <div>
     <h1 class="text-red-200 ">To-Do List</h1>
-    <input 
-    value={input}
-    placeholder="Enter task"
-    onChange={(e)=> setInput(e.target.value)}
+    <input
+      value={input}
+      placeholder="Enter task"
+      onChange={(e) => setInput(e.target.value)}
     />
     <button onClick={handleTaskbutton}>Add Task</button>
     <ul>
-    {tasks.map((task, index)=>(
-        <li key={index}>{task}  <span><button onClick={()=>deleteButton(index)}>X</button></span></li>
-    ))}
+      {tasks.map((task, index) => (
+        <li key={index}>
+          {task}{" "}
+          <span>
+            <button onClick={() => deleteButton(index)}>X</button>
+          </span>
+          <span>
+            <button onClick={handleComplete}>✅</button>
+          </span>
+        </li>
+      ))}
     </ul>
     <p> Number of task to be Done: {count}</p>
-    </div>
-)
+    <p> Number of Complete : {complete}</p>
+  </div>
+);
 }
