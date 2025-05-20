b60f01dea561e1858e1692ff2b4fd34d7c525c92
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-20 11:17:14 -0700

    added more css

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 export default function TodoList(){
     const [input, setInput] = useState([]);
-    const [tasks, setTasks] = useState(["laundry", "groceries" ]);
+    const [tasks, setTasks] = useState(["laundry", "groceries", " picking kid from school" ]);
     const [count, setCount] = useState(2)
     const[complete, setComplete] = useState(0)
     const [incomplete, setIncomplete] = useState(0)
@@ -49,36 +49,34 @@ function handleIncomplete(index){
 }
      
 return (
-  <div class=" w-100 rounded-lg shadow-md p-6">
+  <div class=" justify-center mt-10 mr-auto ml-auto w-10/12 rounded-lg shadow-md p-6 md:w-1/2 bg-white">
     <h1 class="text-center font-bold text-[30px]">To-Do List</h1>
     <input
       value={input}
       placeholder="Enter task"
       onChange={(e) => setInput(e.target.value)}
-      class=" p-1 border-2 rounded-md w-[260px] mr-4"
+      class=" p-1 border-2 rounded-md w-3/4 mr-4"
     />
-    <button onClick={handleTaskbutton} class="bg-black text-white rounded-md p-1">Add Task</button>
+    <button onClick={handleTaskbutton} class="bg-black text-white rounded-md p-1 hover:text-black hover:bg-white">Add Task</button>
     {errors && <p>{errors}</p>}
+   <div class="mt-6">
     <ul>
       {tasks.map((task, index) => (
-        <li key={index} class=" w-66 shadow-md p-4">
+        <li key={index} class=" shadow-md p-2">
           {task}{" "}
-          <span>
-            <button onClick={() => deleteButton(index)}>X</button>
-          </span>
-          <span>
-            <button onClick={() => handleComplete(index)}>✅</button>
-          </span>
-          <span>
-            <button onClick={() => handleIncomplete(index)}>⭕</button>
+            <span class="flex justify-end gap-1">
+            <button onClick={() => deleteButton(index)} class="text-[30px] transition-transform duration-300 hover:rotate-[15deg]">X</button>
+            <button onClick={() => handleComplete(index)} class="text-[20px] transition-transform duration-300 hover:rotate-[20deg]">✅</button>
+            <button onClick={() => handleIncomplete(index)} class="text-[20px]">⭕</button>
           </span>
         </li>
       ))}
     </ul>
-    <div class="p-6 ">
-    <p> Number of task to be Done: {count}</p>
-    <p> Number of Complete Tasks : {complete}</p>
-    <p>Number of Incompleted Tasks : {incomplete}</p>
+    </div>
+    <div class="p-3">
+    <p class="text-left font-bold"> Number of task to be Done: {count}</p>
+    <p class="text-green-900 font-bold"> Number of Complete Tasks : {complete}</p>
+    <p class=" text-red-600 font-bold">Number of Incompleted Tasks : {incomplete}</p>
     </div>
   </div>
 );
